604d36584c06c9e66a16a20c543e0d9438cf79cc
feldip44 <44462078+feldip44@users.noreply.github.com>
2018-11-06 15:10:48 +0100

    Add files via upload

---
+++ nauka5.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main()
+{
+    int liczba;
+    do
+    {
+    printf("Podaj liczbe 5\n");
+    scanf("%d",&liczba);
+    }while(liczba!=5);
+    printf("Podano dobra liczbe");
+    getchar();
+    return 0;
+
+}
