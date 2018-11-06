89b3a9729882cb22445eb3705f454200d4e910dd
feldip44 <44462078+feldip44@users.noreply.github.com>
2018-11-06 15:21:58 +0100

    Dodano kolejne polecenie
    Dodano polecenie - podaj liczbe calkowita wieksza od 0.

--- nauka5.c
+++ nauka5.c
@@ -7,8 +7,21 @@ int main()
     printf("Podaj liczbe 5\n");
     scanf("%d",&liczba);
     }while(liczba!=5);
-    printf("Podano dobra liczbe");
+    printf("Podano dobra liczbe\n");
+    {
+         printf("Podaj liczbe calkowita wieksza od 0\n");
+ scanf("%d",&liczba);
+ if (liczba>0)
+ {
+     printf("Liczba prawidlowa\n");
+ }
+ else
+ {
+     printf("Liczba nieprawidlowa\n");
+ }
+    }
     getchar();
     return 0;
 
 }
+
